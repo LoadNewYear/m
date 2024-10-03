da9550350285af3f8789e509f5be294901aa7239
LoadNewYear <145248490+LoadNewYear@users.noreply.github.com>
2024-10-03 09:45:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ document.getElementById('updateButton').addEventListener('click', function() {
     const goal = 10000
     const savedValue = parseFloat(document.getElementById('saved').value);
     saved = saved + savedValue
+    const remaining = goal - saved;
+        
+    document.getElementById('totalSaved').innerText = `Total Saved: $${saved.toFixed(2)}`;
+    document.getElementById('remaining').innerText = `Remaining to Save: $${remaining >= 0 ? remaining.toFixed(2) : 0}`;
     
-    if (!isNaN(saved)) {
-        const remaining = goal - saved;
+    // Optional: Reset the input fields after updating
+    document.getElementById('goal').value = '';
+    document.getElementById('saved').value = '';
+    //if (!isNaN(saved)) {
         
-        document.getElementById('totalSaved').innerText = `Total Saved: $${saved.toFixed(2)}`;
-        document.getElementById('remaining').innerText = `Remaining to Save: $${remaining >= 0 ? remaining.toFixed(2) : 0}`;
-        
-        // Optional: Reset the input fields after updating
-        document.getElementById('goal').value = '';
-        document.getElementById('saved').value = '';
-    } else {
-        alert('Please enter valid numbersaved amount.');
-    }
+    //} else {
+    //    alert('Please enter valid numbersaved amount.');
+    //}
 });
 
